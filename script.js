3cc674c1482bed10e5ece51324b609511dfae6c4
Balaji Nagmoti <balajinagmotibca01@gmail.com>
2025-10-26 22:30:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,14 +34,7 @@
                 url: " https://balaji-bca2005.github.io/color.github.io/",
                 desc: "Select The color for website ",
                 logo: "iamges/color.png"
-            },
-          {
-            title: "Calculator",
-            url:"https://balaji-bca2005.github.io/Calculator/",
-            desc: "Use the web based calculator",
-            logo: "iamges/calc.jpg"
-              
-          }
+            }
         ];
 
         function createCard(p) {
@@ -167,6 +160,7 @@ darkBtn.onclick = function() {
     localStorage.setItem('darkMode', isDark);
 
 };
+
 
 
 
